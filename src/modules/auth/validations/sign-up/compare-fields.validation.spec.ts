3d273002a9baa723f2d0707f61cab07230ae1092
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-03-21 19:22:51 -0300

    test: ensure CompareFieldsValidation returns void if field matches fieldToCompare

--- src/modules/auth/validations/sign-up/compare-fields.validation.spec.ts
+++ src/modules/auth/validations/sign-up/compare-fields.validation.spec.ts
@@ -11,4 +11,13 @@ describe('CompareFieldsValidation', () => {
       sut.validate({ any_field: 'value', other_field: 'different_value' })
     ).toThrow();
   });
+  test('ensure CompareFieldsValidation returns void if field matches fieldToCompare', () => {
+    const field = 'any_field';
+    const fieldToCompare = 'other_field';
+    const sut = new CompareFieldsValidation(field, fieldToCompare);
+
+    const result = sut.validate({ any_field: 'value', other_field: 'value' });
+
+    expect(result).toBeFalsy();
+  });
 });
