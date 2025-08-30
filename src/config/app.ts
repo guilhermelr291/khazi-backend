0a083ac218d81b78e4dd0a6f6ce3fd7e8388e935
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-08-30 13:46:50 -0300

    feat: add cors

--- src/config/app.ts
+++ src/config/app.ts
@@ -2,8 +2,17 @@ import express from 'express';
 import setupRoutes from './routes';
 import { errorHandler } from '../common/middlewares/error-handler-middleware';
 import { checkAuth } from '../common/middlewares/check-auth-middleware';
-
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN || 'http://localhost:8080',
+  })
+);
+
 app.use(express.json());
 app.use(checkAuth);
 setupRoutes(app);
